b6037d4ceb4cbe0719c88dd13bc0746c77a7f072
vaxherd <111935183+vaxherd@users.noreply.github.com>
2024-03-20 19:37:34 +0900

    Fix handling of zone changes
    We get PLAYER_ENTERING_WORLD on entering any new zone, not just login
    (and conversely PLAYER_LEAVING_WORLD on leaving a zone).

--- WoWXIV.lua
+++ WoWXIV.lua
@@ -1,16 +1,16 @@
 WoWXIV = {}
 
 WoWXIV.startup_frame = CreateFrame("Frame", "WoWXIV_StartupFrame")
-WoWXIV.startup_frame:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_ENTERING_WORLD" then
+WoWXIV.startup_frame:RegisterEvent("ADDON_LOADED")
+WoWXIV.startup_frame:SetScript("OnEvent", function(self, event, arg1, ...)
+    if event == "ADDON_LOADED" and arg1 == "WoWXIV" then
         WoWXIV.Config.Create()
         WoWXIV.Gamepad.Init()
 
         WoWXIV.BuffBar.Create()
         --WoWXIV.HateList.Create() --FIXME semi-broken, needs to deal with unaggroed mobs and mobs going out of range among other things
         WoWXIV.PartyList.Create()
-        WoWXIV.TargetBar.Create()
         WoWXIV.SlashCmd.Init()
+        WoWXIV.TargetBar.Create()
     end
 end)
-WoWXIV.startup_frame:RegisterEvent("PLAYER_ENTERING_WORLD")
