f76a12bc1a816f09aa20610fb97fda166a1a5d51
vaxherd <111935183+vaxherd@users.noreply.github.com>
2024-04-22 05:17:55 +0900

    Various improvements to gamepad quest item trigger

--- gamepad.lua
+++ gamepad.lua
@@ -35,31 +35,74 @@ local QuestItemButton = class()
 -- friendly NPC, but is neither "helpful" nor "harmful", while Rusziona's
 -- Whistle (202293) from Little Scales Daycare quest "What's a Duck?"
 -- (72459) is marked "helpful" but requires the player to be targeted in
--- order to fire.  Ideally we would just call
--- UseQuestLogSpecialItem(log_index), but that's a protected function and
--- there's no secure action wrapper for it (as of 10.2.6), so for the
--- meantime we record specific items whose required targets we know and
--- use fallback logic for others.
+-- order to fire.  Yet other items explicitly require a target of "none".
+-- Ideally we would just call UseQuestLogSpecialItem(log_index), but that's
+-- a protected function and there's no secure action wrapper for it (as of
+-- 10.2.6), so for the meantime we record specific items whose required
+-- targets we know and use fallback logic for others.
 local ITEM_TARGET = {
+    -- Benthic Sealant (56160: Plug the Geysers)
+    [168482] = "none",
+    -- Loremaster's Notebook (58471: Aggressive Notation)
+    [174197] = "target",
     -- Primordial Muck (59808: Muck It Up)
     [177880] = "player",
+    -- Resonating Anima Core (60609: Who Devours the Devourers?)
+    [180008] = "none",
+    -- Resonating Anima Mote (60609: Who Devours the Devourers?)
+    [180009] = "none",
+    -- Torch (60770: Squish and Burn)
+    [180274] = "none",
     -- Aqueous Material Accumulator (61189: Further Gelatinous Research)
     [180876] = "player",
-    -- Fae Flute (61717: Gormling Piper: Tranquil Pools [and others])
-    [182189] = "",  -- FIXME: does not seem to work with any target
+    -- Gormling in a Bag (61394: Gormling Toss: Tranquil Pools)
+    [181284] = "none",
+    -- Fae Flute (61717: Gormling Piper: Tranquil Pools)
+    [182189] = "player",
     -- Assassin's Soulcloak (61765: Words of Warding)
     [182303] = "player",
+    -- Gormling in a Bag (62051: Gormling Toss: Spirit Glen)
+    [182600] = "none",
     -- Niya's Staff (63840: They Grow Up So Quickly)
     [186089] = "target",
+    -- Ornithological Medical Kit (66071: Flying Rocs)
+    [189384] = "target",
+    -- Feather-Plucker 3300 (65374: It's Plucking Time)
+    [189454] = "target",
+    -- The Chirpsnide Auto-Excre-Collector (65490: Explosive Excrement)
+    [190188] = "player",
+    -- Im-PECK-able Screechflight Disguise (65778: Screechflight Potluck)
+    [191681] = "player",
+    -- Im-PECK-able Screechflight Disguise v2 (66299: The Awaited Egg-splosion)
+    [191763] = "player",
     -- Arch Instructor's Wand (66489: Setting the Defense)
     [192471] = "target",
+    -- Borrowed Breath (66180: Wake the Ancients)
+    [192555] = "player",
+    -- Trusty Dragonkin Rake (72991: Warm Dragonfruit Pie)
+    [193826] = "target",
+    -- Bottled Water Elemental (66998: Fighting Fire with... Water)
+    [194441] = "none",
     -- Rusziona's Whistle (72459: What's a Duck?)
     [202293] = "player",
+}
+
+-- Special cases for quests which don't have items listed but really should.
+local QUEST_ITEM = {
+    -- Ardenweald world quest: Who Devours the Devourers?
+    [60609] = {
+        map = 1565,  -- Ardenweald
+        items = {
+            180008,  -- Resonating Anima Core
+            180009,  -- Resonating Anima Mote
+        }
+    }
 }
 
 function QuestItemButton:__constructor()
     self.item = nil
     self.pending_update = false
+    self.last_update = 0
 
     local f = CreateFrame("Button", "WoWXIV_QuestItemButton", nil,
                           "SecureActionButtonTemplate")
@@ -73,35 +116,56 @@ function QuestItemButton:__constructor()
                        "CLICK WoWXIV_QuestItemButton:LeftButton")
 
     f:RegisterUnitEvent("UNIT_QUEST_LOG_CHANGED", "player")
+    f:RegisterEvent("BAG_UPDATE")  -- for QUEST_ITEM quests
     f:SetScript("OnEvent", function() self:UpdateQuestItem() end)
     self:UpdateQuestItem()
 end
 
-function QuestItemButton:UpdateQuestItem(is_retry)
+function QuestItemButton:UpdateQuestItem(event, is_retry)
     if not is_retry and self.pending_update then return end
-    if InCombatLockdown() then
+    local now = GetTime()
+    if InCombatLockdown() or now - self.last_update < 1 then
         self.pending_update = true
-        C_Timer.After(1, function() self:UpdateQuestItem(true) end)
+        C_Timer.After(1, function() self:UpdateQuestItem(event, true) end)
         return
     end
     self.pending_update = false
+    self.last_update = now
 
     local item = nil
-    for index = 1, C_QuestLog.GetNumQuestLogEntries() do
-        local link, icon, charges, show_when_complete =
-            GetQuestLogSpecialItemInfo(index)
-        if link then
-            -- GetItemInfoFromHyperlink() is defined in Blizzard's
-            -- SharedXML/LinkUtil.lua
-            item = GetItemInfoFromHyperlink(link)
-            if item then break end
+
+    for quest, info in pairs(QUEST_ITEM) do
+        if C_QuestLog.IsOnQuest(quest) then
+            if C_Map.GetBestMapForUnit("player") == info.map then
+                for _, quest_item in ipairs(info.items) do
+                    if C_Item.GetItemCount(quest_item) > 0 then
+                        item = quest_item
+                        break
+                    end
+                end
+                if item then break end
+            end
+        end
+    end
+    if not item then
+        for index = 1, C_QuestLog.GetNumQuestLogEntries() do
+            local link, icon, charges, show_when_complete =
+                GetQuestLogSpecialItemInfo(index)
+            if link then
+                -- GetItemInfoFromHyperlink() is defined in Blizzard's
+                -- SharedXML/LinkUtil.lua
+                item = GetItemInfoFromHyperlink(link)
+                if item then break end
+            end
         end
     end
     if item then
-        -- We can't activate an item by ID (the ID would be interpreted as
-        -- an inventory slot index), so we need to set the name instead.
-        local name = GetItemInfo(item)
-        self.frame:SetAttribute("item", name)
+        -- Note that we have to use the "item:" format rather than just
+        -- the numeric item ID, because the latter would be treated as an
+        -- inventory index instead.  We can't use the item name because
+        -- that fails when multiple items have the same name, such as the
+        -- quest items for the various gormling world quests in Ardenweald.
+        self.frame:SetAttribute("item", "item:"..item)
         local known_target = ITEM_TARGET[item]
         if known_target then
             if #known_target then
