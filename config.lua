8be5f2276e045d534b5f9b20b1cf312251875c7c
vaxherd <111935183+vaxherd@users.noreply.github.com>
2024-04-24 05:04:34 +0900

    Minor refactor of settings panel

--- config.lua
+++ config.lua
@@ -32,7 +32,7 @@ local CONFIG_DEFAULT = {
     map_show_coords_worldmap = true,
 
     -- Party list: where to use role/class colors
-    partylist_colors = nil,
+    partylist_colors = "none",
     -- Party list: when to use narrow format
     partylist_narrow_condition = "never",
 
@@ -57,141 +57,161 @@ local CONFIG_DEFAULT = {
 
 ------------------------------------------------------------------------
 
-local ConfigFrame = class()
+-- Abstract base class for config panel elements.  Lua technically
+-- doesn't need an explicit abstract base class because everything is
+-- resolved dynamically, but this serves as documentation of the shared
+-- API for all elements.
 
-function ConfigFrame:AddHeader(text)
-    local f = self.native_frame
-    local label = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightLarge")
-    self.y = self.y - 30
-    label:SetPoint("TOPLEFT", self.x, self.y)
-    label:SetTextScale(1.2)
-    label:SetText(text)
-    self.y = self.y - 15
-    return label
+local ConfigPanelElement = class()
+
+-- Return the desired vertical spacing between this element and the next.
+function ConfigPanelElement:GetSpacing()
+    return 0
 end
 
-function ConfigFrame:AddText(text)
-    local f = self.native_frame
-    local label = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    self.y = self.y - 25
-    label:SetPoint("TOPLEFT", self.x, self.y)
-    label:SetPoint("TOPRIGHT", -self.x, self.y)
-    label:SetJustifyH("LEFT")
-    label:SetSpacing(3)
-    label:SetTextScale(1.1)
-    label:SetText(text)
-    -- FIXME: This is fundamentally broken because we don't know how wide
-    -- the frame will be until it's sized when the options window is first
-    -- opened, and therefore we don't know how tall it will end up being.
-    -- We can get away with this for now because this is only used once
-    -- and at the very bottom of the config frame (for the about text).
-    self.y = self.y - 70
-    return label
-end
+------------------------------------------------------------------------
 
-function ConfigFrame:AddComment(text)
-    local f = self.native_frame
-    local label = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    self.y = self.y - 6
-    label:SetPoint("TOPLEFT", self.x+40, self.y)
-    label:SetTextColor(1, 0.5, 0)
-    label:SetText(text)
-    self.y = self.y - 10
-    return label
-end
+local CPCheckButton = class(ConfigPanelElement)
 
-function ConfigFrame:AddHorizontalBar(text)
-    local f = self.native_frame
-    local texture = f:CreateTexture(nil, "ARTWORK")
-    self.y = self.y - 10
-    texture:SetPoint("LEFT", f, "TOPLEFT", self.x, self.y)
-    texture:SetPoint("RIGHT", f, "TOPRIGHT", 0, self.y)
-    texture:SetHeight(1.5)
-    texture:SetAtlas("Options_HorizontalDivider")
-    self.y = self.y - 10
-    return texture
-end
+function CPCheckButton:__constructor(panel, x, y, text, setting, on_change,
+                                     depends_on)
+    self.setting = setting
+    self.on_change = on_change
+    self.depends_on = depends_on
+    self.dependents = {}
 
-function ConfigFrame:AddCheckButton(text, setting, on_change, depends_on)
     local indent = depends_on and 1 or 0
-    local f = self.native_frame
-    local button = CreateFrame("CheckButton", nil, f, "UICheckButtonTemplate")
-    self.y = self.y - 10
-    button:SetPoint("TOPLEFT", self.x+10+30*indent, self.y)
+    local button = CreateFrame("CheckButton", nil, panel.frame,
+                               "UICheckButtonTemplate")
+    self.button = button
+    button:SetPoint("TOPLEFT", x+30*indent, y)
     button.text:SetTextScale(1.25)
     button.text:SetText(text)
-    self.y = self.y - 20
     button:SetChecked(WoWXIV_config[setting])
-    function button:SetSensitive(sensitive)  -- SetEnable() plus color change
-        self:SetEnabled(sensitive)
-        -- SetEnabled() doesn't change the text color, so we have to do
-        -- that manually.
-        self.text:SetTextColor(
-            (sensitive and NORMAL_FONT_COLOR or DISABLED_FONT_COLOR):GetRGB())
-    end
-    button:SetScript("OnClick", function(self)
-        local new_value = not WoWXIV_config[setting]
-        WoWXIV_config[setting] = new_value
-        self:SetChecked(new_value)
-        for _, dep in ipairs(self.dependents) do
-            dep:SetSensitive(new_value)
-        end
-        if on_change then on_change(new_value) end
-    end)
-    button.dependents = {}
+    button:SetScript("OnClick", function() self:OnClick() end)
     if depends_on then
-        local depends_on_button = self.buttons[depends_on]
-        assert(depends_on_button)
-        tinsert(depends_on_button.dependents, button)
-        button:SetSensitive(WoWXIV_config[depends_on])
+        depends_on:AddDependent(self)
+        self:SetSensitive(WoWXIV_config[depends_on.setting])
     end
-    self.buttons[setting] = button
-    return button
 end
 
-function ConfigFrame:AddRadioHeader(text)
-    local f = self.native_frame
-    local label = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    self.y = self.y - 20
-    label:SetPoint("TOPLEFT", self.x+15, self.y)
-    label:SetText(text)
-    self.y = self.y - 10
-    return label
+function CPCheckButton:GetSpacing()
+    return 30
 end
 
-function ConfigFrame:AddRadioButton(text, setting, value, on_change)
-    local f = self.native_frame
-    local button = CreateFrame("CheckButton", nil, f, "UIRadioButtonTemplate")
-    self.y = self.y - 10
-    button:SetPoint("TOPLEFT", self.x+35, self.y)
+function CPCheckButton:AddDependent(dependent)
+    tinsert(self.dependents, dependent)
+end
+
+function CPCheckButton:SetSensitive(sensitive) -- SetEnable() plus color change
+    local button = self.button
+    self.button:SetEnabled(sensitive)
+    -- SetEnabled() doesn't change the text color, so we have to do
+    -- that manually.
+    self.button.text:SetTextColor(
+        (sensitive and NORMAL_FONT_COLOR or DISABLED_FONT_COLOR):GetRGB())
+end
+
+function CPCheckButton:SetChecked(checked)
+    checked = checked and true or false  -- Force to boolean type.
+    self.button:SetChecked(checked)
+    for _, dep in ipairs(self.dependents) do
+        dep:SetSensitive(checked)
+    end
+    WoWXIV_config[self.setting] = checked
+    if self.on_change then
+        self.on_change(checked)
+    end
+end
+
+function CPCheckButton:OnClick()
+    self:SetChecked(not WoWXIV_config[self.setting])
+end
+
+------------------------------------------------------------------------
+
+-- This class is not a visible element, but serves to group all radio
+-- buttons for a single setting to ensure that only one is checked.
+local CPRadioGroup = class()
+
+function CPRadioGroup:__constructor(setting, on_change)
+    self.setting = setting
+    self.on_change = on_change
+    self.buttons = {}
+end
+
+-- button must be a CPRadioButton.
+function CPRadioGroup:AddButton(button)
+    self.buttons[button.value] = button
+end
+
+function CPRadioGroup:SetValue(value)
+    local value_button = self.buttons[value]
+    if not value_button then
+        error(("Invalid value for radio group %s: %s"):format(
+                  self.setting, value))
+        return
+    end
+    value_button:SetChecked(true)
+    for _, button in pairs(self.buttons) do
+        if button ~= value_button then
+            button:SetChecked(false)
+        end
+    end
+    WoWXIV_config[self.setting] = value
+    if self.on_change then
+        self.on_change(value)
+    end
+end
+
+------------------------------------------------------------------------
+
+local CPRadioButton = class(ConfigPanelElement)
+
+-- Automatically adds the button to the given CPRadioGroup.
+function CPRadioButton:__constructor(panel, x, y, text, group, value)
+    self.group = group
+    self.value = value
+
+    self.on_change = on_change
+    self.depends_on = depends_on
+    self.dependents = {}
+
+    local button = CreateFrame("CheckButton", nil, panel.frame,
+                               "UIRadioButtonTemplate")
+    self.button = button
+    button:SetPoint("TOPLEFT", x, y)
     button.text:SetTextScale(1.25)
     button.text:SetText(text)
-    self.y = self.y - 10
-    button:SetChecked(WoWXIV_config[setting] == value)
-    button.WoWXIV_value = value
-    f.WoWXIV_radio_buttons = f.WoWXIV_radio_buttons or {}
-    f.WoWXIV_radio_buttons[setting] = f.WoWXIV_radio_buttons[setting] or {}
-    tinsert(f.WoWXIV_radio_buttons[setting], button)
-    button:SetScript("OnClick", function(self)
-        WoWXIV_config[setting] = value
-        self:SetChecked(true)
-        for _, other in ipairs(f.WoWXIV_radio_buttons[setting]) do
-            if other.WoWXIV_value ~= value then
-                other:SetChecked(false)
-            end
-        end
-        if on_change then on_change(new_value) end
-    end)
-    return button
+    button:SetChecked(WoWXIV_config[group.setting] == value)
+    button:SetScript("OnClick", function() self:OnClick() end)
+
+    group:AddButton(self)
 end
 
-function ConfigFrame:__constructor()
+function CPRadioButton:GetSpacing()
+    return 20
+end
+
+function CPRadioButton:SetChecked(checked)
+    self.button:SetChecked(checked)
+end
+
+function CPRadioButton:OnClick()
+    self.group:SetValue(self.value)
+end
+
+------------------------------------------------------------------------
+
+local ConfigPanel = class()
+
+function ConfigPanel:__constructor()
     self.buttons = {}
 
-    local f = CreateFrame("Frame", "WoWXIV_ConfigFrame")
-    self.native_frame = f
+    local f = CreateFrame("Frame", "WoWXIV_ConfigPanel")
+    self.frame = f
     self.x = 10
-    self.y = 10  -- Assuming an initial header.
+    self.y = 0
 
     self:AddHeader("Buff/debuff bar settings")
     self:AddCheckButton("Show distance for Dragon Glyph Resonance",
@@ -217,29 +237,18 @@ function ConfigFrame:__constructor()
                         function(enable) WoWXIV.Map.SetShowCoords(enable, WoWXIV_config["map_show_coords_minimap"]) end)
 
     self:AddHeader("Party list settings")
-    self:AddRadioHeader("Role/class coloring:",
-                        "partylist_role_bg", WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("None", "partylist_colors", nil,
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("Role color in background",
-                        "partylist_colors", "role",
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("Role color in background, class color in name",
-                        "partylist_colors", "role+class",
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("Class color in background",
-                        "partylist_colors", "class",
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioHeader("Use narrow format (omit mana and limit buffs/debuffs):")
-    self:AddRadioButton("Never", "partylist_narrow_condition", "never",
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("Always", "partylist_narrow_condition", "always",
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("Only in raids", "partylist_narrow_condition", "raid",
-                        WoWXIV.PartyList.Refresh)
-    self:AddRadioButton("Only in raids with 21+ members",
-                        "partylist_narrow_condition", "raidlarge",
-                        WoWXIV.PartyList.Refresh)
+    self:AddRadioGroup("Role/class coloring:",
+                       "partylist_colors", WoWXIV.PartyList.Refresh,
+                       "None", "none",
+                       "Role color in background", "role",
+                       "Role color in background, class color in name", "role+class",
+                       "Class color in background", "class")
+    self:AddRadioGroup("Use narrow format (omit mana and limit buffs/debuffs):",
+                       "partylist_narrow_condition", WoWXIV.PartyList.Refresh,
+                       "Never", "never",
+                       "Always", "always",
+                       "Only in raids", "raid",
+                       "Only in raids with 21+ members", "raidlarge")
 
     self:AddHeader("Target bar settings")
     self:AddCheckButton("Hide native target frames |cffff0000(requires reload)|r",
@@ -279,6 +288,84 @@ function ConfigFrame:__constructor()
     f:SetHeight(-self.y + 10)
 end
 
+function ConfigPanel:AddHeader(text)
+    local f = self.frame
+    local label = f:CreateFontString(nil, "ARTWORK", "GameFontHighlightLarge")
+    self.y = self.y - 20
+    label:SetPoint("TOPLEFT", self.x, self.y)
+    label:SetTextScale(1.2)
+    label:SetText(text)
+    self.y = self.y - 25
+    return label
+end
+
+function ConfigPanel:AddText(text)
+    local f = self.frame
+    local label = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    self.y = self.y - 15
+    label:SetPoint("TOPLEFT", self.x, self.y)
+    label:SetPoint("TOPRIGHT", -self.x, self.y)
+    label:SetJustifyH("LEFT")
+    label:SetSpacing(3)
+    label:SetTextScale(1.1)
+    label:SetText(text)
+    -- FIXME: This is fundamentally broken because we don't know how wide
+    -- the frame will be until it's sized when the options window is first
+    -- opened, and therefore we don't know how tall it will end up being.
+    -- We can get away with this for now because this is only used once
+    -- and at the very bottom of the config frame (for the about text).
+    self.y = self.y - 80
+    return label
+end
+
+function ConfigPanel:AddComment(text)
+    local f = self.frame
+    local label = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    label:SetPoint("TOPLEFT", self.x+40, self.y+4)
+    label:SetTextColor(1, 0.5, 0)
+    label:SetText(text)
+    self.y = self.y - 16
+    return label
+end
+
+function ConfigPanel:AddHorizontalBar(text)
+    local f = self.frame
+    local texture = f:CreateTexture(nil, "ARTWORK")
+    texture:SetPoint("LEFT", f, "TOPLEFT", self.x, self.y)
+    texture:SetPoint("RIGHT", f, "TOPRIGHT", 0, self.y)
+    texture:SetHeight(1.5)
+    texture:SetAtlas("Options_HorizontalDivider")
+    self.y = self.y - 20
+    return texture
+end
+
+function ConfigPanel:AddCheckButton(text, setting, on_change, depends_on)
+    depends_on = depends_on and self.buttons[depends_on]
+    local button = CPCheckButton(self, self.x+10, self.y,
+                                 text, setting, on_change, depends_on)
+    self.y = self.y - button:GetSpacing()
+    self.buttons[setting] = button
+end
+
+-- Call as: AddRadioGroup(header, setting, on_change,
+--                        text1, value1, [text2, value2...])
+function ConfigPanel:AddRadioGroup(header, setting, on_change, ...)
+    local f = self.frame
+    local label = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    self.y = self.y - 10
+    label:SetPoint("TOPLEFT", self.x+15, self.y)
+    label:SetText(header)
+    self.y = self.y - 20
+
+    local group = CPRadioGroup(setting, on_change)
+    for i = 1, select("#",...), 2 do
+        local text, value = select(i,...)
+        local button = CPRadioButton(self, self.x+35, self.y,
+                                     text, group, value)
+        self.y = self.y - button:GetSpacing()
+    end
+end
+
 ------------------------------------------------------------------------
 
 -- Initialize configuration data and create the configuration window.
@@ -289,9 +376,9 @@ function WoWXIV.Config.Create()
         end
     end
 
-    local config_frame = ConfigFrame()
-    WoWXIV.Config.frame = config_frame
-    local f = config_frame.native_frame
+    local config_panel = ConfigPanel()
+    WoWXIV.Config.panel = config_paneel
+    local f = config_panel.frame
 
     local container = CreateFrame("ScrollFrame", "WoWXIV_ConfigScroller", nil,
                                   "UIPanelScrollFrameTemplate")
