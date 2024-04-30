d16f38335d4350fc10c73081f237703757a50e31
vaxherd <111935183+vaxherd@users.noreply.github.com>
2024-05-01 05:20:04 +0900

    Cache bank and void storage content for /isearch results
    To make it actually more useful than the backpack search box.

--- slashcmd.lua
+++ slashcmd.lua
@@ -1,6 +1,8 @@
 local _, WoWXIV = ...
 WoWXIV.SlashCmd = {}
 
+local tinsert = tinsert
+
 local FCT = function(...)
     FCT = WoWXIV.FormatColoredText
     return FCT(...)
@@ -198,240 +200,21 @@ end)
 
 ---------------- /itemsearch (/isearch, /is)
 
--- WoW provides the C_Container.GetBagName() API to get the bag item name
--- for a bag slot, but (1) that doesn't let us differentiate between
--- multiple bags of the same name and (2) it doesn't work for special bags
--- like the main bank bag, so we use our own names here (but still append
--- the bag item name for player-created bags).
-function BAGDEF(id, name, append_bagname, in_combined)
-    return {id = id, name = name, append_bagname = append_bagname,
-            in_combined = in_combined or false}
-end
-local BAGS = {
-    BAGDEF(Enum.BagIndex.Backpack, "Backpack", false, true),
-    BAGDEF(Enum.BagIndex.Bag_1, "Bag 1", true, true),
-    BAGDEF(Enum.BagIndex.Bag_2, "Bag 2", true, true),
-    BAGDEF(Enum.BagIndex.Bag_3, "Bag 3", true, true),
-    BAGDEF(Enum.BagIndex.Bag_4, "Bag 4", true, true),
-    BAGDEF(Enum.BagIndex.ReagentBag, "Reagent Bag", false),
-    BAGDEF(Enum.BagIndex.Bank, "Bank", false),
-    BAGDEF(Enum.BagIndex.BankBag_1, "Bank Bag 1", true),
-    BAGDEF(Enum.BagIndex.BankBag_2, "Bank Bag 2", true),
-    BAGDEF(Enum.BagIndex.BankBag_3, "Bank Bag 3", true),
-    BAGDEF(Enum.BagIndex.BankBag_4, "Bank Bag 4", true),
-    BAGDEF(Enum.BagIndex.BankBag_5, "Bank Bag 5", true),
-    BAGDEF(Enum.BagIndex.BankBag_6, "Bank Bag 6", true),
-    BAGDEF(Enum.BagIndex.BankBag_7, "Bank Bag 7", true),
-    BAGDEF(Enum.BagIndex.Reagentbank, "Bank Reagent Bag", false),
-}
-
--- Void storage is handled by a separate subsystem, which just needs the
--- tab and slot index.
-local VOID_MAX_TABS = 2
-local VOID_MAX_SLOTS = 80  -- per tab
-
--- For equipment, names are available as global constants, but again they
--- don't provide a way to distinguish between multiple slots of the same type.
-function EQUIPDEF(id, name)
-    return {id = id, name = name}
-end
-local EQUIPS = {
-    EQUIPDEF("HEADSLOT", "Head"),
-    EQUIPDEF("NECKSLOT", "Neck"),
-    EQUIPDEF("SHOULDERSLOT", "Shoulders"),
-    EQUIPDEF("BACKSLOT", "Back"),
-    EQUIPDEF("CHESTSLOT", "Chest"),
-    EQUIPDEF("SHIRTSLOT", "Shirt"),
-    EQUIPDEF("TABARDSLOT", "Tabard"),
-    EQUIPDEF("WRISTSLOT", "Wrists"),
-    EQUIPDEF("HANDSSLOT", "Hands"),
-    EQUIPDEF("WAISTSLOT", "Waist"),
-    EQUIPDEF("LEGSSLOT", "Legs"),
-    EQUIPDEF("FEETSLOT", "Feet"),
-    EQUIPDEF("FINGER0SLOT", "Finger 1"),
-    EQUIPDEF("FINGER1SLOT", "Finger 2"),
-    EQUIPDEF("TRINKET0SLOT", "Trinket 1"),
-    EQUIPDEF("TRINKET1SLOT", "Trinket 2"),
-    EQUIPDEF("MAINHANDSLOT", "Main Hand"),
-    EQUIPDEF("SECONDARYHANDSLOT", "Off Hand"),
-    EQUIPDEF("PROF0TOOLSLOT", "Profession 1 Tool"),
-    EQUIPDEF("PROF0GEAR0SLOT", "Profession 1 Accessory 1"),
-    EQUIPDEF("PROF0GEAR1SLOT", "Profession 1 Accessory 2"),
-    EQUIPDEF("PROF1TOOLSLOT", "Profession 2 Tool"),
-    EQUIPDEF("PROF1GEAR0SLOT", "Profession 2 Accessory 1"),
-    EQUIPDEF("PROF1GEAR1SLOT", "Profession 2 Accessory 2"),
-    EQUIPDEF("COOKINGTOOLSLOT", "Cooking Tool"),
-    EQUIPDEF("COOKINGGEAR0SLOT", "Cooking Accessory"),
-    EQUIPDEF("FISHINGTOOLSLOT", "Fishing Rod"),
-    EQUIPDEF("FISHINGGEAR0SLOT", "Fishing Accessory 1"),
-    EQUIPDEF("FISHINGGEAR1SLOT", "Fishing Accessory 2"),
-}
-
-local function SlotsString(slots)
-    local s
-    if #slots == 1 then
-        s = " slot " .. slots[1]
-    else
-        s = " slots " .. slots[1]
-        if #slots == 2 then
-            s = s .. " and " .. slots[2]
-        else
-            for i = 2, #slots-1 do
-                s = s .. ", " .. slots[i]
-            end
-            s = s .. ", and " .. slots[#slots]
-        end
-    end
-    return s
-end
-
--- FIXME: surely there must be a way to get the combined state directly
--- rather than this hack of listening for changes (which doesn't work on
--- module load anyway)?
-local isearch_combined_bags = false
--- Note that this code is all deferred, so we can create the frame inline
--- rather than wrapping it in a function to be called later.
-local isearch_event_frame = WoWXIV.CreateEventFrame("WoWXIV_IsearchEventFrame")
-isearch_event_frame:RegisterEvent("USE_COMBINED_BAGS_CHANGED")
-function isearch_event_frame:USE_COMBINED_BAGS_CHANGED(enabled)
-    isearch_combined_bags = enabled
-end
-
 DefineCommand("itemsearch", {"isearch", "is"}, Green("ItemName"),
-        {"Searches your inventory and equipment for an item.",
-         "The bank UI must be open to search bank bags other than the bank reagent bag.",
-         "Similarly, the void storage UI must be open to search void storage.",
-         "Bag items equipped in inventory or bank slots are not listed.",
-         "When using a combined backpack, slots count from the bottom right: slot 10 is the bottom left corner, 31 is the right side of the 4th row from the bottom, and so on.",
-         "",
-         "Unlike the search box in the default backpack UI, this command does not search item descriptions, so it can be useful in narrowing down the search when you know the item's name.",
-         "",
-         "Examples:",
-         "    "..Yellow("/itemsearch Heart of Azeroth"),
-         "     → Shows where your Heart of Azeroth is stored or equipped.",
-         "    "..Yellow("/itemsearch wildercloth"),
-         "     → Finds all items with \"Wildercloth\" in the name, excluding any Wildercloth Bags equipped as inventory or bank bags."},
-function(arg)
-    if not arg or arg == "" then
-        print(Red("No item name given. Try \"/? itemsearch\" for help."))
-        return
-    end
-
-    print("Searching for " .. Yellow(arg))
-    search_key = arg:lower()
-
-    local results = {}
-
-    if isearch_combined_bags then
-        local found_slots = {}
-        local offset = 0
-        for i = #BAGS, 1, -1 do
-            local bag = BAGS[i]
-            if bag.in_combined then
-                local bag_size = C_Container.GetContainerNumSlots(bag.id)
-                for slot = 1, bag_size do
-                    local loc = ItemLocation:CreateFromBagAndSlot(bag.id, slot)
-                    if loc and loc:IsValid() then
-                        local name = C_Item.GetItemName(loc)
-                        if name:lower():find(search_key, 1, true) then
-                            found_slots[name] = found_slots[name] or {}
-                            tinsert(found_slots[name], offset + slot)
-                        end
-                    end
-                end
-                offset = offset + bag_size
-            end
-        end
-        -- Careful here: the "#" operator only works on lists, i.e.
-        -- tables whose keys are consecutive integers starting at 1.
-        -- Lua doesn't seem to have a direct way to test an arbitrary
-        -- table for emptiness, but next() will do the trick.
-        -- Lua really is not a great language, but it's what we've got...
-        if next(found_slots, nil) then
-            for item, slots in pairs(found_slots) do
-                local s = SlotsString(slots)
-                tinsert(results, {item, " found in " .. Blue("Combined Backpack" .. s)})
-            end
-        end
-    end
-
-    for _, bag in ipairs(BAGS) do
-        if not (isearch_combined_bags and bag.in_combined) then
-            local found_slots = {}
-            local bag_size = C_Container.GetContainerNumSlots(bag.id)
-            for slot = 1, bag_size do
-                local loc = ItemLocation:CreateFromBagAndSlot(bag.id, slot)
-                if loc and loc:IsValid() then
-                    local name = C_Item.GetItemName(loc)
-                    if name:lower():find(search_key, 1, true) then
-                        found_slots[name] = found_slots[name] or {}
-                        tinsert(found_slots[name], slot)
-                    end
-                end
-            end
-            if next(found_slots, nil) then
-                local bag_name = bag.name
-                if bag.append_bagname then
-                    bag_name = bag_name .. " (" .. C_Container.GetBagName(bag.id) .. ")"
-                end
-                for item, slots in pairs(found_slots) do
-                    local s = SlotsString(slots)
-                    tinsert(results, {item, " found in " .. Blue(bag_name .. s)})
-                end
-            end
-        end
-    end
-
-    if CanUseVoidStorage() and IsVoidStorageReady() then
-        for tab = 1, VOID_MAX_TABS do
-            local found_slots = {}
-            for slot = 1, VOID_MAX_SLOTS do
-                local item = GetVoidItemInfo(tab, slot)
-                if item then
-                    local name = C_Item.GetItemInfo(item)
-                    if name and name:lower():find(search_key, 1, true) then
-                        found_slots[name] = found_slots[name] or {}
-                        tinsert(found_slots[name], slot)
-                    end
-                end
-            end
-            if next(found_slots, nil) then
-                local bag_name = "Void Storage tab " .. tab
-                for item, slots in pairs(found_slots) do
-                    local s = SlotsString(slots)
-                    tinsert(results, {item, " found in " .. Blue(bag_name .. s)})
-                end
-            end
-        end
-    end
-
-    for _, slot in ipairs(EQUIPS) do
-        local slot_info = GetInventorySlotInfo(slot.id)
-        assert(slot_info)
-        local loc = ItemLocation:CreateFromEquipmentSlot(slot_info)
-        if loc and loc:IsValid() then
-            local name = C_Item.GetItemName(loc)
-            if name:lower():find(search_key, 1, true) then
-                tinsert(results, {name, " equipped on " .. Blue(slot.name)})
-            end
-        end
-    end
-
-    if #results > 0 then
-        -- Lua apparently does not have a stable sort method, so we have
-        -- to ensure stability manually.
-        for i, v in ipairs(results) do
-            tinsert(v, i)
-        end
-        table.sort(results, function(a,b) return a[1] < b[1] or (a[1] == b[1] and a[#a] < b[#b]) end)
-        for _, result in ipairs(results) do
-            local name, location = unpack(result)
-            print(" → " .. Yellow(name) .. location)
-        end
-    else
-        print(Red(" → No matching items found."))
-    end
-end)
+              {"Searches your inventory and equipment for an item.",
+               "Bag items equipped in inventory or bank slots are not listed.",
+               "When using a combined backpack, slots count from the bottom right: slot 10 is the bottom left corner, 31 is the right side of the 4th row from the bottom, and so on.",
+               "",
+               "Unlike the search box in the default backpack UI, this command does not search item descriptions, so it can be useful in narrowing down the search when you know the item's name.",
+               "",
+               "Bank bags (other than the reagent bank) and void storage contents may be out of date if you log in from multiple computers. Visit the relevant NPC to update the cached data.",
+               "",
+               "Examples:",
+               "    "..Yellow("/itemsearch Heart of Azeroth"),
+               "     → Shows where your Heart of Azeroth is stored or equipped.",
+               "    "..Yellow("/itemsearch wildercloth"),
+               "     → Finds all items with \"Wildercloth\" in the name, excluding any Wildercloth Bags equipped as inventory or bank bags."},
+              WoWXIV.isearch)
 
 ---------------- /wowxiv (/xiv)
 
