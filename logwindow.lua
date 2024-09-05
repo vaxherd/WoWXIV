53f2f2f9c17fc58dcfb58f16a44e9aec78458869
vaxherd <111935183+vaxherd@users.noreply.github.com>
2024-09-05 11:36:58 +0900

    Drop log window testing hack to keep native chat frame around
    Code is left in for now, wrapped by a debug flag test.

--- logwindow.lua
+++ logwindow.lua
@@ -119,6 +119,9 @@ local MESSAGE_TYPES = {
     VoiceText = {"CHAT_MSG_VOICE_TEXT"},
 }
 
+-- For testing: set to true to keep the native chat frame visible
+local KEEP_NATIVE_FRAME = false
+
 --------------------------------------------------------------------------
 
 local Tab = class()
@@ -285,8 +288,11 @@ function LogWindow:__constructor()
                               UIParent)
     self.frame = frame
     frame:SetSize(430, 120)
-    -- FIXME: put above the native chat frame until we're done testing
-    frame:SetPoint("BOTTOMLEFT", GeneralDockManager, "TOPLEFT", 0, 24)
+    if not KEEP_NATIVE_FRAME then
+        frame:SetPoint("BOTTOMLEFT", ChatFrame1EditBox, "TOPLEFT", 5, 17)
+    else
+        frame:SetPoint("BOTTOMLEFT", GeneralDockManager, "TOPLEFT", 0, 24)
+    end
 
     frame:SetTimeVisible(2*60)
     frame:SetMaxLines(WoWXIV_config["logwindow_history"])
@@ -499,37 +505,37 @@ function LogWindow:AddMessage(event, text, r, g, b)
     r = r or 1
     g = g or 1
     b = b or 1
-    -- FIXME: temp hack until we turn off native chat frame events
-    self.last_message = self.last_message or {0, 0, 0, 0, 0}
-    self.saved_message = self.saved_message or {0, 0, 0, 0, 0}
-    if event ~= "_" and text == self.saved_message[2] and r == self.saved_message[3] and g == self.saved_message[4] and b == self.saved_message[5] then
-        self.saved_message[2] = 0
-    end
-    if self.saved_message[2] ~= 0 then
-        local saved_event, saved_text, saved_r, saved_g, saved_b = unpack(self.saved_message)
-        if saved_event == "_" then saved_event = "-" end
-        self.saved_message[2] = 0
-        self:AddMessage(saved_event, saved_text, saved_r, saved_g, saved_b)
-    end
-    if event == nil then return end  -- from RunNextFrame call below
-    if event == "_" then
-        if not (text == self.last_message[2] and r == self.last_message[3] and g == self.last_message[4] and b == self.last_message[5]) then
-            self.saved_message[1] = event
-            self.saved_message[2] = text
-            self.saved_message[3] = r
-            self.saved_message[4] = g
-            self.saved_message[5] = b
-            RunNextFrame(function() self:AddMessage(nil, text=="" and "-" or "") end)
+    if not KEEP_NATIVE_FRAME then
+        self.last_message = self.last_message or {0, 0, 0, 0, 0}
+        self.saved_message = self.saved_message or {0, 0, 0, 0, 0}
+        if event ~= "_" and text == self.saved_message[2] and r == self.saved_message[3] and g == self.saved_message[4] and b == self.saved_message[5] then
+            self.saved_message[2] = 0
         end
-        return
-    end
-    self.last_message[1] = event
-    self.last_message[2] = text
-    self.last_message[3] = r
-    self.last_message[4] = g
-    self.last_message[5] = b
-    RunNextFrame(function() self.last_message[2] = 0 end)
-    --FIXME end hack
+        if self.saved_message[2] ~= 0 then
+            local saved_event, saved_text, saved_r, saved_g, saved_b = unpack(self.saved_message)
+            if saved_event == "_" then saved_event = "-" end
+            self.saved_message[2] = 0
+            self:AddMessage(saved_event, saved_text, saved_r, saved_g, saved_b)
+        end
+        if event == nil then return end  -- from RunNextFrame call below
+        if event == "_" then
+            if not (text == self.last_message[2] and r == self.last_message[3] and g == self.last_message[4] and b == self.last_message[5]) then
+                self.saved_message[1] = event
+                self.saved_message[2] = text
+                self.saved_message[3] = r
+                self.saved_message[4] = g
+                self.saved_message[5] = b
+                RunNextFrame(function() self:AddMessage(nil, text=="" and "-" or "") end)
+            end
+            return
+        end
+        self.last_message[1] = event
+        self.last_message[2] = text
+        self.last_message[3] = r
+        self.last_message[4] = g
+        self.last_message[5] = b
+        RunNextFrame(function() self.last_message[2] = 0 end)
+    end  -- if not KEEP_NATIVE_FRAME
     if self.tab_bar:GetActiveTab():Filter(event, text) then
         self.frame:AddMessage(text, r, g, b)
     end
@@ -550,12 +556,13 @@ function WoWXIV.LogWindow.Create()
     WoWXIV.LogWindow.PruneHistory()
 
     WoWXIV.LogWindow.window = LogWindow()
-    if false then  -- FIXME: keep native chat window around while testing
+    if not KEEP_NATIVE_FRAME then
         WoWXIV.HideBlizzardFrame(GeneralDockManager)
         local index = 1
         while _G["ChatFrame"..index] do
             local frame = _G["ChatFrame"..index]
             WoWXIV.HideBlizzardFrame(frame)
+            index = index + 1
         end
         DEFAULT_CHAT_FRAME.AddMessage = function(frame, ...)
             WoWXIV.LogWindow.window:AddMessage(...)
