11a28aefc6573d3449ce2b874078662ebf88747c
vaxherd <111935183+vaxherd@users.noreply.github.com>
2025-05-28 22:15:26 +0900

    Update power type list for combat log

--- logwindow.lua
+++ logwindow.lua
@@ -461,13 +461,13 @@ local COMBAT_POWER_TEXT = {
     [Enum.PowerType.SoulShards] = SOUL_SHARDS,
     [Enum.PowerType.LunarPower] = LUNAR_POWER,
     [Enum.PowerType.HolyPower] = HOLY_POWER,
-    [Enum.PowerType.Maelstrom] = MAELSTROM_POWER,
+    [Enum.PowerType.Alternate] = ALTERNATE,
+    [Enum.PowerType.Maelstrom] = MAELSTROM,
     [Enum.PowerType.Chi] = CHI_POWER,
-    [Enum.PowerType.Insanity] = INSANITY_POWER,
-    [Enum.PowerType.ArcaneCharges] = ARCANE_CHARGES_POWER,
+    [Enum.PowerType.Insanity] = INSANITY,
+    [Enum.PowerType.ArcaneCharges] = ARCANE_CHARGES,
     [Enum.PowerType.Fury] = FURY,
     [Enum.PowerType.Pain] = PAIN,
-    [Enum.PowerType.Essence] = POWER_TYPE_ESSENCE,
     -- Not in Blizzard code:
     [Enum.PowerType.AlternateMount] = "Vigor",
 }
