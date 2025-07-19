vaxherd/WoWXIV

commit d96f8771f34e4c2bde1469cb58d72eeb3a1d0801

Reduce taint errors
Looks like a lighter hand may be the better choice after all...

targetbar.lua
@@ -395,6 +395,10 @@ function WoWXIV.TargetBar.Create()
         WoWXIV.HideBlizzardFrame(TargetFrame)
         WoWXIV.HideBlizzardFrame(FocusFrame)
         WoWXIV.HideBlizzardFrame(BossTargetFrameContainer)
+        -- Because BossTargetFrameContainer is hidden/shown during combat,
+        -- we can't rely on our Show/SetShown hooks to re-hide it.
+        -- Instead, we suppress the event handling which would show it.
+        Boss1TargetFrame:UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
     end
     if WoWXIV_config["targetbar_move_top_center"] then
         -- Put it about halfway between the hotbars and menu bar.

buffbar.lua
@@ -80,14 +80,14 @@ function WoWXIV.BuffBar.Create()
     f:SetHeight(40*4)
     f:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -250, -10)
 
-    local x = UIParent:GetWidth() - 250
     --WoWXIV.BuffBar.buff_bar = WoWXIV.UI.AuraBar(
     --    "HELPFUL", "BOTTOMRIGHT", 20, 2, f, 0, 80)
     WoWXIV.BuffBar.buff_bar = PlayerBuffBar(f, 0, 80)
     WoWXIV.BuffBar.debuff_bar = WoWXIV.UI.AuraBar(
         "HARMFUL", "TOPRIGHT", 20, 1, f, 0, -80)
     WoWXIV.BuffBar.debuff_bar:SetUnit("player")
-    -- FIXME: not sure how to separate out misc buffs from others
+    -- FIXME: not sure how to separate out misc buffs from others (and we can't
+    -- anyway until SecureAuraHeaderTemplate gives us a better filter option)
     --WoWXIV.BuffBar.misc_bar = WoWXIV.UI.AuraBar(
     --    "MISC", "TOPRIGHT", 20, 1, f, 0, -120)
 end

buffbar.xml
@@ -1,11 +1,8 @@
 <Ui xmlns="http://www.blizzard.com/wow/ui/">
-  <Button name="PlayerBuffTemplate" inherits="SecureActionButtonTemplate" virtual="true">
+  <Button name="PlayerBuffTemplate" inherits="SecureActionButtonTemplate" registerForClicks="RightButtonDown" virtual="true">
     <Size x="24" y="37"/>
     <Attributes>
       <Attribute name="type2" value="cancelaura"/>
     </Attributes>
-    <Scripts>
-      <OnLoad>self:RegisterForClicks("RightButtonDown")</OnLoad>
-    </Scripts>
   </Button>
 </Ui>

gamepad/menucursor/menucursor.lua
@@ -429,6 +429,8 @@ function Cursor:UpdateCursor(in_combat)
         end
     end
 
+    if in_combat and not entering_combat then return end
+
     ClearOverrideBindings(self)
     -- Any access to WoWXIV_config here will taint execution in StaticPopup
     -- frames, which can break some common game actions such as item

util.lua
@@ -128,21 +128,9 @@ end
 function WoWXIV.HideBlizzardFrame(frame)
     frame:UnregisterAllEvents()
     frame:Hide()
-    function frame:Show() end
-    function frame:SetShown() end
-    function frame:Update() end
-    -- Also cleared to prevent taint warnings:
-    function frame:Hide() end
-    function frame:SetWidth() end
-    function frame:SetHeight() end
-    function frame:SetSize() end
-    if frame ~= BuffFrame and frame ~= DebuffFrame then  -- _causes_ taint errors on logout in 11.2.0
-        function frame:SetPoint() end
-    end
-    -- Needed to avoid errors from EditModeManagerFrame:
-    function frame:GetPoint(n)
-        if n==1 then return "TOPLEFT",UIParent,"TOPLEFT",0,0 end
-    end
+    hooksecurefunc(frame, "Show", frame.Hide)
+    hooksecurefunc(frame, "SetShown",
+                   function(f,show) if show then f:Hide() end end)
 end
 
 ------------------------------------------------------------------------

commit 658c1dc91f1ea1169ebc83475171bd0e1badbdcb

Fix target frame appearing on client scene change
Regression introduced in d96f8771.

targetbar.lua
@@ -399,6 +399,16 @@ function WoWXIV.TargetBar.Create()
         -- we can't rely on our Show/SetShown hooks to re-hide it.
         -- Instead, we suppress the event handling which would show it.
         Boss1TargetFrame:UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+        -- Avoid the target frame reappearing when changing "scenes"
+        -- (e.g. during the "History of Corrupt" world quest in Bastion).
+        -- This introduces taint to a global function, but only at the
+        -- end of the secure execution sequence for all call sites, so
+        -- it's safe (as of 11.2.0).
+        local function nop() end
+        local dummy_TargetFrame = {Hide = nop, Update = nop}
+        local fenv = setmetatable({TargetFrame = dummy_TargetFrame},
+                                  {__index = _G})
+        setfenv(UpdateUIElementsForClientScene, fenv)
     end
     if WoWXIV_config["targetbar_move_top_center"] then
         -- Put it about halfway between the hotbars and menu bar.