vaxherd/WoWXIV

commit d4d1dbbb9faa5a2bb7e22dbceba5004d9114c4e5

Make use of new list type
Mostly low-hanging ipairs() fruit for now.  Potentially some utility
with remove() as well (see CPGamepadBinding:OnGamePadButtonUp()).

Remove util.lconcat() since list addition serves that purpose now.

config.lua
@@ -2,6 +2,7 @@ local _, WoWXIV = ...
 WoWXIV.Config = {}
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 
 local floor = math.floor
 local function round(x) return math.floor(x+0.5) end
@@ -165,7 +166,7 @@ function CPCheckButton:__constructor(panel, x, y, text, setting_or_state,
     end
     self.on_change = on_change
     self.depends_on = depends_on
-    self.dependents = {}
+    self.dependents = list()
 
     local indent = depends_on and 1 or 0
     local button = CreateFrame("CheckButton", nil, panel.frame,
@@ -187,7 +188,7 @@ function CPCheckButton:GetSpacing()
 end
 
 function CPCheckButton:AddDependent(dependent)
-    tinsert(self.dependents, dependent)
+    self.dependents:append(dependent)
 end
 
 function CPCheckButton:SetSensitive(sensitive) -- SetEnable() plus color change
@@ -209,7 +210,7 @@ function CPCheckButton:OnClick()
     -- This is called _after_ the UIButton state has been toggled, so we
     -- only need to perform appropriate updates.
     local checked = self.button:GetChecked()
-    for _, dep in ipairs(self.dependents) do
+    for _dep in self.dependents do
         dep:SetSensitive(checked)
     end
     if self.setting then
@@ -267,7 +268,6 @@ function CPRadioButton:__constructor(panel, x, y, text, group, value)
 
     self.on_change = on_change
     self.depends_on = depends_on
-    self.dependents = {}
 
     local button = CreateFrame("CheckButton", nil, panel.frame,
                                "UIRadioButtonTemplate")
@@ -395,7 +395,7 @@ function CPGamepadBinding:Activate()
                             self:OnGamePadButtonUp(...)
                         end)
     self.frame:SetScript("OnKeyUp", function(_,...) self:OnKeyUp(...) end)
-    self.buttons_down = {}
+    self.buttons_down = list()
     self.new_binding = ""
     self.button_text:SetTextColor(WHITE_FONT_COLOR:GetRGB())
     self.button_text:SetText("Waiting...")
@@ -422,10 +422,10 @@ function CPGamepadBinding:OnGamePadButtonDown(button)
     -- We only accept the first (possibly modified) button, but we track
     -- all pressed buttons and only release the input lock once all
     -- buttons have been released.
-    for _,b in ipairs(self.buttons_down) do
+    for b in self.buttons_down do
         assert(b ~= button)
     end
-    tinsert(self.buttons_down, button)
+    self.buttons_down:append(button)
     if #self.buttons_down == 1 then
         local modifiers = ""
         if IsAltKeyDown() then modifiers = modifiers .. "ALT-" end
@@ -442,15 +442,7 @@ function CPGamepadBinding:OnGamePadButtonDown(button)
 end
 
 function CPGamepadBinding:OnGamePadButtonUp(button)
-    local found = false
-    for i,b in ipairs(self.buttons_down) do
-        if b == button then
-            tremove(self.buttons_down, i)
-            found = true
-            break
-        end
-    end
-    assert(found)
+    self.buttons_down:remove(button)
     if #self.buttons_down == 0 and self.new_binding ~= "" then
         self:Deactivate()
         self:SetBinding(self.new_binding)

flytext.lua
@@ -2,6 +2,7 @@ local _, WoWXIV = ...
 WoWXIV.FlyText = {}
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 local set = WoWXIV.set
 local Frame = WoWXIV.Frame
 
@@ -317,7 +318,7 @@ local FlyTextManager = class()
 
 function FlyTextManager:__constructor(parent)
     self.enabled = true
-    self.texts = {}
+    self.texts = list()
     self.dot = {}
     self.hot = {}
     self.last_left = 0
@@ -534,7 +535,7 @@ function FlyTextManager:OnLootItem(event, msg)
 end
 
 -- For duplicate currency event check, see below.
-local CURRENCY_PAIRS = {
+local CURRENCY_PAIRS = list(
     {2805, 2806},  -- Whelpling's Awakened Crest
     {2807, 2808},  -- Drake's Awakened Crest
     {2810, 2809},  -- Wyrm's Awakened Crest
@@ -551,10 +552,10 @@ local CURRENCY_PAIRS = {
     {3286, 3287},  -- Carved Ethereal Crest
     {3288, 3289},  -- Runed Ethereal Crest
     {3290, 3291},  -- Gilded Ethereal Crest
-    {3252, 3372},  -- Bronze (Legion Remix)
-}
+    {3252, 3372}   -- Bronze (Legion Remix)
+)
 local CURRENCY_PAIR_MAP = {}
-for _, pair in ipairs(CURRENCY_PAIRS) do
+for pair in CURRENCY_PAIRS do
     CURRENCY_PAIR_MAP[pair[1]] = pair[2]
     CURRENCY_PAIR_MAP[pair[2]] = pair[1]
 end
@@ -650,11 +651,11 @@ function FlyTextManager:AddText(text, left_side)
     local min_offset = 16
     if dt*dy < min_offset then
         local time_offset = (min_offset - dt*dy) / dy
-        for _, t in ipairs(self.texts) do
+        for t in self.texts do
             t:Push(time_offset)
         end
     end
-    tinsert(self.texts, text)
+    self.texts:append(text)
 end
 
 function FlyTextManager:OnUpdate()
@@ -676,7 +677,7 @@ function FlyTextManager:OnUpdate()
     if self.hot then
         for unit, amount in pairs(self.hot) do
             text = FlyText(FLYTEXT_HEAL_PASSIVE, unit, amount)
-            tinsert(self.texts, text)
+            self.texts:append(text)
         end
         self.hot = nil
     end
@@ -687,7 +688,7 @@ function FlyTextManager:OnUpdate()
         if texts[i]:OnUpdate() then
             i = i + 1
         else
-            tremove(texts, i)
+            texts:pop(i)
         end
     end
 end

gamepad/commandmenu.lua
@@ -3,6 +3,7 @@ WoWXIV.Gamepad = WoWXIV.Gamepad or {}
 local Gamepad = WoWXIV.Gamepad
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 local Button = WoWXIV.Button
 local Frame = WoWXIV.Frame
 
@@ -129,7 +130,7 @@ function CommandMenuColumn:__constructor(parent, title)
     highlight_tex:SetAlpha(0.4)
     highlight:Hide()
 
-    self.items = {}
+    self.items = list()
     self.default_item = 1
     self.position = 1
     self:Close()
@@ -154,7 +155,7 @@ function CommandMenuColumn:AddItem(name, help, func, is_default)
 end
 
 function CommandMenuColumn:SetItemEnabled(name, enabled, disabled_reason)
-    for _, item in ipairs(self.items) do
+    for item in self.items do
         if item.name == name then
             item.button:SetEnabled(enabled)
             item.help_suffix = not enabled and disabled_reason or nil
@@ -165,7 +166,7 @@ function CommandMenuColumn:SetItemEnabled(name, enabled, disabled_reason)
 end
 
 function CommandMenuColumn:IsItemEnabled(name)
-    for _, item in ipairs(self.items) do
+    for item in self.items do
         if item.name == name then
             return item.button:IsEnabled()
         end
@@ -174,7 +175,7 @@ function CommandMenuColumn:IsItemEnabled(name)
 end
 
 function CommandMenuColumn:SetItemSecureAction(name, action)
-    for _, item in ipairs(self.items) do
+    for item in self.items do
         if item.name == name then
             item.secure_action = action
             return
@@ -319,7 +320,7 @@ function CommandMenuColumn:OnUpdate()
         self.menu:ClearAllPoints()
         self.menu:SetPoint("TOPLEFT", self.highlight, "TOPLEFT",
                            0, self.menu_offset)
-        for _, item in ipairs(self.items) do
+        for item in self.items do
             local above = max(item.button:GetTop() - self.highlight:GetTop(), 0)
             item.button:SetAlpha(1 - 0.6 * min(above/40, 1))
         end
@@ -569,14 +570,14 @@ function CommandMenu:__constructor()
     WoWXIV.SetUITexture(background, 252, 256, 64, 112)
     background:SetTextureSliceMargins(1, 16, 1, 16)
 
-    local columns = {CharacterColumn(self),
-                     ContentColumn(self),
-                     CollectionsColumn(self),
-                     CommunicationColumn(self),
-                     SystemColumn(self)}
+    local columns = list(CharacterColumn(self),
+                         ContentColumn(self),
+                         CollectionsColumn(self),
+                         CommunicationColumn(self),
+                         SystemColumn(self))
     self.columns = columns
     local title_width = 0
-    for _, column in ipairs(columns) do
+    for column in columns do
         local width = column:GetTitleWidth()
         title_width = max(title_width, width)
     end
@@ -628,7 +629,7 @@ function CommandMenu:OnHide()
     self:UpdateBindings(false)
     self:SetScript("OnUpdate", nil)
     self.brm:StopRepeat()
-    for _, column in ipairs(self.columns) do
+    for column in self.columns do
         column:Close()
     end
 end

gamepad/gamepad.lua
@@ -3,6 +3,7 @@ WoWXIV.Gamepad = WoWXIV.Gamepad or {}
 local Gamepad = WoWXIV.Gamepad
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 local Button = WoWXIV.Button
 
 local strsub = string.sub
@@ -57,17 +58,17 @@ end
 
 -- Pass (setting,command) pairs to bind.
 function GamepadBoundButton:__constructor(...)
-    self.bindings = {}
+    self.bindings = list()
     for i = 1, select("#",...), 2 do
         local setting, command = select(i, ...)
-        tinsert(self.bindings, {setting, command})
+        self.bindings:append({setting, command})
     end
     self:UpdateBinding()
 end
 
 function GamepadBoundButton:UpdateBinding()
     ClearOverrideBindings(self)
-    for _, binding in ipairs(self.bindings) do
+    for binding in self.bindings do
         SetOverrideBinding(self, false,
                            WoWXIV_config[binding[1]], binding[2])
     end
@@ -193,19 +194,19 @@ function GamePadListener:OnGamePadStick(stick, x, y)
 
     -- Handle scrolling text frames.  There are two types of these, so we
     -- need two lists.
-    local SCROLL_FRAMES = {  -- ScrollFrameTemplate
+    local SCROLL_FRAMES = list(  -- ScrollFrameTemplate
         ItemTextScrollFrame,
         OpenMailScrollFrame,
         QuestDetailScrollFrame,
         QuestMapDetailsScrollFrame,
         QuestRewardScrollFrame,
-        "ItemSocketingScrollFrame",
-    }
-    local SCROLLBOX_FRAMES = {  -- WowScrollBoxList
-        GossipFrame.GreetingPanel.ScrollBox,
-    }
+        "ItemSocketingScrollFrame"
+    )
+    local SCROLLBOX_FRAMES = list(  -- WowScrollBoxList
+        GossipFrame.GreetingPanel.ScrollBox
+    )
     local scroll_frame, scroll_current, scroll_SetScroll
-    for _, frame in ipairs(SCROLL_FRAMES) do
+    for frame in SCROLL_FRAMES do
         if type(frame) == "string" then
             frame = _G[frame]
         end
@@ -223,7 +224,7 @@ function GamePadListener:OnGamePadStick(stick, x, y)
             break
         end
     end
-    for _, frame in ipairs(SCROLLBOX_FRAMES) do
+    for frame in SCROLLBOX_FRAMES do
         if frame:IsVisible() then
             local limit = frame:GetDerivedScrollRange()
             if limit >= 0.01 then

gamepad/questitem.lua
@@ -5,6 +5,7 @@ local Gamepad = WoWXIV.Gamepad
 assert(Gamepad.GamepadBoundButton)  -- Ensure proper load order.
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 
 local GameTooltip = GameTooltip
 local GetItemCount = C_Item.GetItemCount
@@ -352,66 +353,66 @@ local QUEST_ITEM = {
     -- manually.
     [11891] = {
         map = 63,  -- Ashenvale
-        items = {
+        items = list(
             35237,  -- Orb of the Crawler
-            35828,  -- Totemic Beacon
-        }
+            35828   -- Totemic Beacon
+        )
     },
     -- Marasmius daily quest: Go Beyond! [Lonely Matriarch]
     [60188] = {
         map = 1565,  -- Ardenweald
-        items = {
-            178464,  -- Discarded Harp
-        }
+        items = list(
+            178464   -- Discarded Harp
+        )
     },
     -- Ardenweald world quest: Who Devours the Devourers?
     [60609] = {
         map = 1565,  -- Ardenweald
-        items = {
+        items = list(
             180008,       -- Resonating Anima Core
-            {180009, 5},  -- Resonating Anima Mote
-        }
+            {180009, 5}   -- Resonating Anima Mote
+        )
     },
     -- Zereth Mortis world quest: Feed the Annelids
     -- The quest properly switches its item as you progress, but we
     -- add this entry to provide a count requirement for Piece of Goop.
     [64960] = {
         map = 1970,  -- Zereth Mortis
-        items = {
+        items = list(
             187816,       -- Irresistible Goop
-            {187820, 6},  -- Piece of Goop
-        }
+            {187820, 6}   -- Piece of Goop
+        )
     },
     -- Waking Shores sidequest: Rapid Fire Plans
     [66439] = {
         map = 2022,  -- Waking Shores
-        items = {
-            {192545, 8},  -- Primal Flame Fragment
-        }
+        items = list(
+            {192545, 8}  -- Primal Flame Fragment
+        )
     },
     -- Isle of Dorn sidequest: Playing in the Mud
     [78755] = {
         map = 2248,  -- Isle of Dorn
-        items = {
-            211483,  -- Frenzied Sand Globule
+        items = list(
+            211483  -- Frenzied Sand Globule
             -- Could also be 211484, Frenzied Water Globule
-        }
+        )
     },
     -- Ringing Deeps world quest: Special Assignment: Shadows Below
     [81691] = {
         map = 2214,  -- Ringing Deeps
-        items = {
-            {224292, 3},  -- Radiant Fuel Shard
-        }
+        items = list(
+            {224292, 3}  -- Radiant Fuel Shard
+        )
     },
     -- Undermine sidequest: Garbage Day
     [84672] = {
         map = 2346,  -- Undermine
-        items = {
+        items = list(
             229805,  -- Last Week's Undermine Inquirer
             229824,  -- Banana Peel
-            229825,  -- Dented Can of Kaja'Cola
-        }
+            229825   -- Dented Can of Kaja'Cola
+        )
     },
 }
 
@@ -427,19 +428,19 @@ local SPELL_PHASE_DIVING = 1214374
 --    - String "Torghast": any Torghast map
 --    - Function: entry is active when function returns a true value
 local ZONE_ITEM = {
-    ["Torghast"] = {  -- Special case because Torghast has so many maps.
+    ["Torghast"] = list(  -- Special case because Torghast has so many maps.
         168035,  -- Mawrat Harness
         170498,  -- Deadsoul Hound Harness
         170499,  -- Maw Seeker Harness
         170540,  -- Ravenous Anima Cell
-        187186,  -- Orb of Deception
-    },
-    [1970] = {  -- Zereth Mortis
-        187908,  -- Firim's Spare Forge-Tap
-    },
-    [function(map) return map==2371 and HasAura(SPELL_PHASE_DIVING) end] = {
-        247882,  -- Phase Regulator
-    },
+        187186   -- Orb of Deception
+    ),
+    [1970] = list(  -- Zereth Mortis
+        187908   -- Firim's Spare Forge-Tap
+    ),
+    [function(map) return map==2371 and HasAura(SPELL_PHASE_DIVING) end] = list(
+        247882   -- Phase Regulator
+    ),
 }
 
 ------------------------------------------------------------------------
@@ -513,7 +514,7 @@ function QuestItemButton:__constructor()
     -- We could theoretically dissect ZoneAbilityFrame and work out exactly
     -- which events trigger show/hide of the frame, but the frame itself
     -- makes for a convenient proxy.
-    for _, name in ipairs({"Show", "Hide", "SetShown"}) do
+    for name in list("Show", "Hide", "SetShown") do
         hooksecurefunc(ZoneAbilityFrame, name,
                        function() self:UpdateQuestItem(true) end)
     end
@@ -753,7 +754,7 @@ function QuestItemButton:IterateQuestItems(predicate)
             end
         end
         if in_zone then
-            for _, item in ipairs(items) do
+            for item in items do
                 if GetItemCount(item) > 0 then
                     index = index + 1
                     if predicate and predicate(item) then
@@ -767,7 +768,7 @@ function QuestItemButton:IterateQuestItems(predicate)
     for quest, info in pairs(QUEST_ITEM) do
         if C_QuestLog.IsOnQuest(quest) then
             if player_map == info.map then
-                for _, quest_item in ipairs(info.items) do
+                for quest_item in info.items do
                     local amount = 1
                     if type(quest_item) == "table" then
                         quest_item, amount = unpack(quest_item)

hatelist.lua
@@ -2,6 +2,7 @@ local _, WoWXIV = ...
 WoWXIV.HateList = {}
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 local Frame = WoWXIV.Frame
 
 local CLM = WoWXIV.CombatLogManager
@@ -153,9 +154,9 @@ function HateList:__allocator()
 end
 
 function HateList:__constructor()
-    self.enemies = {}  -- 1 per enemy slot (all precreated)
-    self.guids = {}    -- Mapping from enemy GUID to enemies[] slot
-    self.unit_not_seen = {}  -- Safety net, see OnPeriodicUpdate()
+    self.enemies = list()        -- 1 per enemy slot (all precreated)
+    self.guids = {}              -- Mapping from enemy GUID to enemies[] slot
+    self.unit_not_seen = list()  -- Safety net, see OnPeriodicUpdate()
 
     self.base_y = -(UIParent:GetHeight()/2) -- May be pushed down by party list
 
@@ -182,8 +183,8 @@ function HateList:__constructor()
 
     for i = 1, 8 do
         local y = -2-27*(i-1)
-        tinsert(self.enemies, Enemy(self, y))
-        tinsert(self.unit_not_seen, 0)
+        self.enemies:append(Enemy(self, y))
+        self.unit_not_seen:append(0)
     end
 
     C_Timer.After(1, function() self:OnPeriodicUpdate() end)
@@ -218,7 +219,7 @@ function HateList:Refresh(rescan)
         self.guids = {}
         self:InternalRefresh(1)
     else
-        for _, enemy in ipairs(self.enemies) do
+        for enemy in self.enemies do
             if enemy:UnitGUID() then
                 enemy:Update(enemy:UnitName())
             end
@@ -255,7 +256,7 @@ function HateList:OnEvent(event, unit)
     if event == "PLAYER_REGEN_ENABLED" then
         -- If we left combat, then by definition we have no aggro.
         assert(not InCombatLockdown())
-        for _, enemy in ipairs(self.enemies) do
+        for enemy in self.enemies do
             enemy:SetUnit(nil)
         end
         self:ResizeFrame(0)

isearch.lua
@@ -1,6 +1,7 @@
 local module_name, WoWXIV = ...
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 local strfind = string.find
 local strstr = function(s1,s2,pos) return strfind(s1,s2,pos,true) end
 local tinsert = tinsert
@@ -183,7 +184,7 @@ end
 local function BAGDEF(getter, cache_id, in_combined)
     return {getter = getter, cache_id = cache_id, in_combined = in_combined}
 end
-local BAGS = {
+local BAGS = list(
     BAGDEF(BagGetter(Enum.BagIndex.Backpack, "Backpack", false), nil, true),
     BAGDEF(BagGetter(Enum.BagIndex.Bag_1, "Bag 1", true), nil, true),
     BAGDEF(BagGetter(Enum.BagIndex.Bag_2, "Bag 2", true), nil, true),
@@ -200,15 +201,15 @@ local BAGS = {
     BAGDEF(BankTabGetter(Enum.BankType.Account, 2, "Warband Bank Tab 2"), "warbank2"),
     BAGDEF(BankTabGetter(Enum.BankType.Account, 3, "Warband Bank Tab 3"), "warbank3"),
     BAGDEF(BankTabGetter(Enum.BankType.Account, 4, "Warband Bank Tab 4"), "warbank4"),
-    BAGDEF(BankTabGetter(Enum.BankType.Account, 5, "Warband Bank Tab 5"), "warbank5"),
-}
+    BAGDEF(BankTabGetter(Enum.BankType.Account, 5, "Warband Bank Tab 5"), "warbank5")
+)
 
 -- For equipment, names are available as global constants, but again they
 -- don't provide a way to distinguish between multiple slots of the same type.
 local function EQUIPDEF(id, name)
     return {id = id, name = name}
 end
-local EQUIPS = {
+local EQUIPS = list(
     EQUIPDEF("HEADSLOT", "Head"),
     EQUIPDEF("NECKSLOT", "Neck"),
     EQUIPDEF("SHOULDERSLOT", "Shoulders"),
@@ -237,8 +238,8 @@ local EQUIPS = {
     EQUIPDEF("COOKINGGEAR0SLOT", "Cooking Accessory"),
     EQUIPDEF("FISHINGTOOLSLOT", "Fishing Rod"),
     EQUIPDEF("FISHINGGEAR0SLOT", "Fishing Accessory 1"),
-    EQUIPDEF("FISHINGGEAR1SLOT", "Fishing Accessory 2"),
-}
+    EQUIPDEF("FISHINGGEAR1SLOT", "Fishing Accessory 2")
+)
 
 local function NameIsMatch(name, search_key)
     return name and strstr(name:lower(), search_key)
@@ -296,7 +297,7 @@ end
 
 isearch_event_frame:RegisterEvent("BAG_UPDATE")
 function isearch_event_frame:BAG_UPDATE(bag_id)
-    for _, bag in ipairs(BAGS) do
+    for bag in BAGS do
         local cache_id = bag.cache_id
         if cache_id and bag.getter:BagID() == bag_id then
             local bag_name, size, content = bag.getter:Get()
@@ -328,7 +329,7 @@ end
 isearch_event_frame:RegisterEvent("BANKFRAME_OPENED")
 function isearch_event_frame:BANKFRAME_OPENED()
     self.bankframe_open = true
-    for _, bag in ipairs(BAGS) do
+    for bag in BAGS do
         local cache_id = bag.cache_id
         if cache_id and strstr(cache_id, "bank") then
             local bag_name, size, content = bag.getter:Get()
@@ -362,9 +363,10 @@ function isearch_event_frame:ADDON_LOADED(name)
     if name == module_name then
         if WoWXIV_isearch_cache.bank0 then
             -- Delete cache data from pre-11.2.0 bank bags and void storage.
-            local tags = {"bank0", "bank1", "bank2", "bank3", "bank4", "bank5",
-                          "bank6", "bank7", "bankR", "void1", "void2"}
-            for _, tag in ipairs(tags) do
+            local tags = list("bank0", "bank1", "bank2", "bank3", "bank4",
+                              "bank5", "bank6", "bank7", "bankR", "void1",
+                              "void2")
+            for tag in tags do
                 WoWXIV_isearch_cache[tag] = nil
                 WoWXIV_isearch_cache[tag.."_name"] = nil
                 WoWXIV_isearch_cache[tag.."_size"] = nil
@@ -386,7 +388,7 @@ function WoWXIV.isearch(arg)
     print("Searching for " .. Yellow(arg))
     search_key = arg:lower()
 
-    local results = {}
+    local results = list()
     local used_cache = false
 
     if is_combined_bags then
@@ -419,12 +421,12 @@ function WoWXIV.isearch(arg)
         if next(found_slots, nil) then
             for item, slots in pairs(found_slots) do
                 local s = SlotsString(slots)
-                tinsert(results, {item, " found in " .. Blue("Combined Backpack" .. s)})
+                results:append({item, " found in " .. Blue("Combined Backpack" .. s)})
             end
         end
     end
 
-    for _, bag in ipairs(BAGS) do
+    for bag in BAGS do
         if not (is_combined_bags and bag.in_combined) then
             local bag_name, size, content = bag.getter:Get()
             local cache_id = bag.cache_id
@@ -475,13 +477,13 @@ function WoWXIV.isearch(arg)
             if next(found_slots, nil) then
                 for item, slots in pairs(found_slots) do
                     local s = SlotsString(slots)
-                    tinsert(results, {item, " found in " .. Blue(bag_name .. s)})
+                    results:append({item, " found in " .. Blue(bag_name .. s)})
                 end
             end
         end
     end
 
-    for _, slot in ipairs(EQUIPS) do
+    for slot in EQUIPS do
         local slot_info = GetInventorySlotInfo(slot.id)
         assert(slot_info)
         local loc = ItemLocation:CreateFromEquipmentSlot(slot_info)
@@ -489,7 +491,7 @@ function WoWXIV.isearch(arg)
             local name = C_Item.GetItemName(loc)
             if NameIsMatch(name, search_key) then
                 local link = C_Item.GetItemLink(loc)
-                tinsert(results, {link, " equipped on " .. Blue(slot.name)})
+                results:append({link, " equipped on " .. Blue(slot.name)})
             end
         end
     end
@@ -500,9 +502,11 @@ function WoWXIV.isearch(arg)
         for i, v in ipairs(results) do
             tinsert(v, i)
         end
-        table.sort(results, function(a,b) return a[1] < b[1] or (a[1] == b[1] and a[#a] < b[#b]) end)
+        results:sort(function(a,b)
+                         return a[1] < b[1] or (a[1] == b[1] and a[#a] < b[#b])
+        end)
         local count = 0
-        for _, result in ipairs(results) do
+        for result in results do
             if count >= MAX_RESULTS then
                 print(Red("More than "..MAX_RESULTS.." results, stopping here."))
                 break

partylist.lua
@@ -2,6 +2,7 @@ local _, WoWXIV = ...
 WoWXIV.PartyList = {}
 
 local class = WoWXIV.class
+local list = WoWXIV.list
 local Button = WoWXIV.Button
 local Frame = WoWXIV.Frame
 
@@ -691,7 +692,7 @@ function PartyCursor:SetPartyList(party_list)
     local cur_unit = f:GetAttribute("cur_unit")
     local have_cur_unit = false
     local unitlist = ""
-    for _, member in ipairs(party_list) do
+    for member in party_list do
         local unit = member:GetUnit()
         unitlist = unitlist .. unit .. " "
         f:SetFrameRef("frame_"..unit, member:GetFrame())
@@ -712,11 +713,10 @@ end
 
 local PartyList = class(Frame)
 
-local PARTY_UNIT_TOKENS = WoWXIV.lconcat(
-    {"player", "vehicle"},
-    WoWXIV.maptn("party%n", 4),
-    {"pet"},
-    WoWXIV.maptn("raid%n", 40))
+local PARTY_UNIT_TOKENS = (list("player", "vehicle") +
+                           WoWXIV.maptn("party%n", 4) +
+                           {"pet"} + 
+                           WoWXIV.maptn("raid%n", 40))
 local PARTY_UNIT_ORDER = {}
 for i, token in ipairs(PARTY_UNIT_TOKENS) do
     PARTY_UNIT_ORDER[token] = i
@@ -763,7 +763,7 @@ function PartyList:__constructor()
     bg2_b:SetVertexColor(0, 0, 0, 1)
     bg2_b:Hide()
 
-    for _, unit in ipairs(PARTY_UNIT_TOKENS) do
+    for unit in PARTY_UNIT_TOKENS do
         self.party[unit] = Member(self, unit)
     end
 
@@ -898,8 +898,8 @@ function PartyList:SetParty(is_retry)
         narrow = false
     end
 
-    local party_order = {}
-    for _, unit in ipairs(PARTY_UNIT_TOKENS) do
+    local party_order = list()
+    for unit in PARTY_UNIT_TOKENS do
         local member = self.party[unit]
         assert(member)
         local id = UnitGUID(unit)
@@ -923,9 +923,9 @@ function PartyList:SetParty(is_retry)
             end
         end
         if id then
-            tinsert(party_order,
-                    {member, unit=="player" and -2 or
-                             unit=="vehicle" and -1 or member:GetRole()})
+            party_order:append(
+                {member, unit=="player" and -2 or
+                         unit=="vehicle" and -1 or member:GetRole()})
         else
             member:Hide()
         end
@@ -934,10 +934,10 @@ function PartyList:SetParty(is_retry)
     local do_sort = WoWXIV_config["partylist_sort"]
     local do_bindkeys = do_sort and WoWXIV_config["partylist_fn_override"]
     if do_sort then
-        table.sort(party_order, function(a,b) return a[2] < b[2] end)
+        party_order:sort(function(a,b) return a[2] < b[2] end)
     end
 
-    local cursor_list = {}
+    local cursor_list = list()
     local col_width = narrow and 228 or 256
     local row_height = Member.HEIGHT
     local col_spacing = col_width + (narrow and 0 or 8)
@@ -945,7 +945,7 @@ function PartyList:SetParty(is_retry)
     local x0, y0 = 0, -4
     local row, col, ncols = 0, 0, 0
     local index = 1
-    for _, v in ipairs(party_order) do
+    for v in party_order do
         local member = v[1]
         member:SetBinding(do_bindkeys, index)
         local x = x0 + col*(col_spacing)
@@ -965,7 +965,7 @@ function PartyList:SetParty(is_retry)
             row = 0
         end
         if unit ~= "vehicle" then
-            tinsert(cursor_list, member)
+            cursor_list:append(member)
             index = index + 1
         end
     end

commit 7edf5b425f6dd871fe698eca89d5e59ae1e7ebaf

Fix error on toggling config settings with dependents
Typo introduced in d4d1dbbb.

config.lua
@@ -210,7 +210,7 @@ function CPCheckButton:OnClick()
     -- This is called _after_ the UIButton state has been toggled, so we
     -- only need to perform appropriate updates.
     local checked = self.button:GetChecked()
-    for _dep in self.dependents do
+    for dep in self.dependents do
         dep:SetSensitive(checked)
     end
     if self.setting then