vaxherd/WoWXIV

commit 3ef35ef50b60a88b0ff9c658a6097cb85e802a48

Fix handling of StackSplitFrame in multi-stack mode (e.g. Lemix token exchange)

gamepad/menucursor/menucursor.lua
@@ -2526,9 +2526,8 @@ function NumberInput:__constructor(editbox, on_change, on_confirm)
     f:SetFrameStrata("TOOLTIP") -- Make sure it's visible above other elements.
     if self.is_StackSplitText then
         f:SetScale(UIParent:GetEffectiveScale()*0.64)
-        f:SetPoint("TOPRIGHT", editbox)
+        f:SetPoint("TOPLEFT", editbox)
         f:SetPoint("BOTTOMRIGHT", editbox)
-        f:SetWidth(72)
     else
         f:SetScale(editbox:GetEffectiveScale())
         -- Don't overlap the money icon in money input boxes (esp. silver).
@@ -2553,6 +2552,20 @@ function NumberInput:__constructor(editbox, on_change, on_confirm)
                            on_click = function() self:ConfirmEdit() end}}
 end
 
+-- Return the current value displayed in the edit box as a number.
+-- Encapsulated in a method to allow subclasses to deal with flavor text
+-- in the box (e.g. StackSplitFrame).
+function NumberInput:GetEditBoxValue()
+    return tonumber(self.editbox:GetText())
+end
+
+-- Modify the given number text as appropriate to display in the edit box.
+-- Encapsulated in a method to allow subclasses to deal with flavor text
+-- in the box (e.g. StackSplitFrame).
+function NumberInput:MakeLabelText(value_str)
+    return value_str
+end
+
 -- Set the rendering scale factor for the input text.  Useful when the
 -- default size doesn't match the size of the original InputBox text.
 function NumberInput:SetTextScale(scale)
@@ -2572,15 +2585,12 @@ function NumberInput:Edit(value_min, value_max)
 
     local editbox = self.editbox
     assert(editbox:IsShown())
-    local old_value = editbox:GetText()
-    assert(old_value)
-
     local r, g, b, a = editbox:GetTextColor()
     self.edittext_alpha = a
     editbox:SetTextColor(r, g, b, 0)
     self.label:SetFont(editbox:GetFont())
-    self.old_value = old_value
-    self.value = tonumber(old_value) or value_min
+    self.old_value = self:GetEditBoxValue() or value_min
+    self.value = self.old_value
     self.value_min = value_min
     self.value_max = value_max
     self.pos = 0
@@ -2596,7 +2606,7 @@ function NumberInput:ConfirmEdit()
         local editbox = self.editbox
         local r, g, b = editbox:GetTextColor()
         editbox:SetTextColor(r, g, b, self.edittext_alpha)
-        self:SetEditBoxText(tostring(self.value))
+        self:SetEditBoxText(self:MakeLabelText(tostring(self.value)))
         if self.on_confirm then self:on_confirm() end
     end
 end
@@ -2609,7 +2619,7 @@ function NumberInput:CancelEdit()
         local editbox = self.editbox
         local r, g, b = editbox:GetTextColor()
         editbox:SetTextColor(r, g, b, self.edittext_alpha)
-        self:SetEditBoxText(self.old_value)
+        self:SetEditBoxText(self:MakeLabelText(tostring(self.old_value)))
     end
 end
 
@@ -2664,7 +2674,7 @@ function NumberInput:UpdateLabel()
     text = (strsub(text, 1, digit-1)
             .. FormatColoredText(strsub(text, digit, digit), 1, 0.75, 0.3)
             .. strsub(text, digit+1))
-    self.label:SetText(text)
+    self.label:SetText(self:MakeLabelText(text))
 end
 
 -- Takes care of also calling the on-change callback, if any.

gamepad/menucursor/StackSplitFrame.lua
@@ -4,24 +4,69 @@ local MenuCursor = WoWXIV.Gamepad.MenuCursor
 
 local class = WoWXIV.class
 
+local strformat = string.format
+local strgsub = string.gsub
+local strmatch = string.match
+local strsub = string.sub
+
 ---------------------------------------------------------------------------
 
 local StackSplitFrameHandler = class(MenuCursor.CoreMenuFrame)
 MenuCursor.Cursor.RegisterFrameHandler(StackSplitFrameHandler)
 
-function StackSplitFrameHandler:__constructor()
-    local StackSplitFrame = StackSplitFrame
-    local StackSplitText = StackSplitFrame.StackSplitText
-    local OkayButton = StackSplitFrame.OkayButton
-    local CancelButton = StackSplitFrame.CancelButton
 
+-- Helper to strip the STACKS format text out of edit box text.
+-- Returns the numeric value embedded in the text.
+local function StripSTACKS(text)
+    if strsub(STACKS, 1, 2) == "%d" then
+        n = tonumber(strmatch(text, "^(%d+)"))
+    elseif strsub(STACKS, #stacks-1) == "%d" then
+        n = tonumber(strmatch(text, "(%d+)$"))
+    end
+    return n or tonumber(text)
+end
+
+
+-- Subclass of NumberInput to handle funky syntax in the input box.
+local StackSplitNumberInput = class(MenuCursor.NumberInput)
+
+function StackSplitNumberInput:__constructor(editbox, on_change, on_confirm,
+                                             parent)
+    __super(self, editbox, on_change, on_confirm)
+    self.parent = parent
+end
+
+function StackSplitNumberInput:GetEditBoxValue()
+    local s = self.editbox:GetText()
+    local n
+    if self.parent.isMultiStack then
+        -- The edit box has flavor text in it, so strip that out.
+        n = StripSTACKS(s)
+    end
+    return n or tonumber(s)
+end
+
+function StackSplitNumberInput:MakeLabelText(value_str)
+    if self.parent.isMultiStack then
+        local s = strgsub(STACKS, "%%d", "%%s")
+        return strformat(s, value_str)
+    end
+    return value_str
+end
+
+
+function StackSplitFrameHandler:__constructor()
     __super(self, StackSplitFrame, MenuCursor.MenuFrame.MODAL)
+    local f = self.frame
+    local StackSplitText = f.StackSplitText
+    local OkayButton = f.OkayButton
+    local CancelButton = f.CancelButton
     self.cancel_func = nil
     self.cancel_button = CancelButton
     -- The frame has convenient increment/decrement buttons, so we just
     -- link to those rather than implementing our own function.
-    self.on_prev_page = StackSplitFrame.LeftButton
-    self.on_next_page = StackSplitFrame.RightButton
+    self.on_prev_page = f.LeftButton
+    self.on_next_page = f.RightButton
 
     self.targets = {
         [StackSplitText] = {
@@ -38,19 +83,41 @@ function StackSplitFrameHandler:__constructor()
             left = OkayButton, right = OkayButton},
     }
 
-    self.quantity_input = MenuCursor.NumberInput(
+    self.quantity_input = StackSplitNumberInput(
         StackSplitText, function() self:OnQuantityChanged() end,
-        function() self:SetTarget(OkayButton) end)
+        function() self:SetTarget(OkayButton) end, f)
 end
 
 function StackSplitFrameHandler:EditQuantity()
+    local f = self.frame
+    assert(type(f.minSplit) == "number")
+    assert(f.minSplit > 0)
+    assert(f.minSplit % 1 == 0)
+    self.unit = f.minSplit  -- Assumed constant during editing.
     self.quantity_input:Edit(1, self.frame.maxStack)
 end
 
 function StackSplitFrameHandler:OnQuantityChanged()
-    StackSplitFrame.split = tonumber(StackSplitFrame.StackSplitText:GetText())
-    StackSplitFrame:UpdateStackText()
-    StackSplitFrame:UpdateStackSplitFrame(StackSplitFrame.maxStack)
+    local f = self.frame
+    local split
+    if self.frame.isMultiStack then
+        split = StripSTACKS(f.StackSplitText:GetText())
+    end
+    if not split then
+        split = tonumber(f.StackSplitText:GetText())
+    end
+    if split then
+        f.split = split * self.unit
+        f:UpdateStackText()
+        -- StackSplitMixin.UpdateStackSplitFrame is obsolete logic which
+        -- hasn't been updated to support isMultiStack , and the parts of
+        -- it which handled updating left/right button state based on the
+        -- input value have now been copy-pasted into the individual input
+        -- handlers (?!) so we have to reimplement that behavior ourselves.
+        --f:UpdateStackSplitFrame(f.maxStack)
+        f.LeftButton:SetEnabled(f.split > StackSplitFrame.minSplit)
+        f.RightButton:SetEnabled(f.split < StackSplitFrame.maxStack)
+    end
 end
 
 ---------------------------------------------------------------------------

commit c28304231b3bd38af03bc70751cd174b28bd57ed

Fix numeric editing in non-multistack StackSplitText
Regression introduced in 3ef35ef5.  The label doesn't perfectly match
the position of the base text, but we won't worry about that for now.

gamepad/menucursor/menucursor.lua
@@ -2526,8 +2526,9 @@ function NumberInput:__constructor(editbox, on_change, on_confirm)
     f:SetFrameStrata("TOOLTIP") -- Make sure it's visible above other elements.
     if self.is_StackSplitText then
         f:SetScale(UIParent:GetEffectiveScale()*0.64)
-        f:SetPoint("TOPLEFT", editbox)
-        f:SetPoint("BOTTOMRIGHT", editbox)
+        -- Label is repositioned on open to match the editbox itself
+        -- (which moves depending on the stacking mode).
+        f:SetHeight(10)  -- Arbitrary, but required for label to be rendered.
     else
         f:SetScale(editbox:GetEffectiveScale())
         -- Don't overlap the money icon in money input boxes (esp. silver).
@@ -2585,6 +2586,22 @@ function NumberInput:Edit(value_min, value_max)
 
     local editbox = self.editbox
     assert(editbox:IsShown())
+    if self.is_StackSplitText then
+        local f = self.frame
+        f:ClearAllPoints()
+        assert(editbox:GetNumPoints() == 1)
+        local point = {editbox:GetPoint(1)}
+        f:SetPoint(unpack(point))
+        local parent_width = editbox:GetParent():GetWidth()
+        if point[1] == "CENTER" then
+            f:SetWidth(parent_width)
+            self.label:SetJustifyH("CENTER")
+        else
+            assert(point[1] == "RIGHT")
+            f:SetWidth(parent_width - 2*abs(point[4] or 0))
+            self.label:SetJustifyH("RIGHT")
+        end
+    end
     local r, g, b, a = editbox:GetTextColor()
     self.edittext_alpha = a
     editbox:SetTextColor(r, g, b, 0)